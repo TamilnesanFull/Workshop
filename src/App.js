34be3c98d6737f1470639adee1252ddc12a19e2d
Tamilnesan <60003547+TamilnesanFull@users.noreply.github.com>
2025-04-08 19:30:42 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import CommentForm from "./components/CommentForm";
+
+const App = () => {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-gray-100 p-6">
+      <CommentForm />
+    </div>
+  );
+};
+
+export default App;
